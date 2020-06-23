209f37e93611e5453dc1d797d4789fcdb07986eb
Krzysztof Zbudniewek <krzysztof.zbudniewek@gmail.com>
2020-06-23 02:35:02 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
     name: "PMKAlamofire",
     dependencies: [
-        .Package(url: "https://github.com/mxcl/PromiseKit.git", majorVersion: 6),
-        .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 5)
+        .Package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.0.0"),
+        .Package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0")
     ],
     exclude: ["Tests"]
 )
